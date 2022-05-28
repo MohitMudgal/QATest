0ebbc665a678660e4166f154f2a5b0f24ea757b3
Prashant Shukla <prashantshukla.its@gmail.com>
2022-05-28 15:28:40 +0530

    Added Junit Test

--- demo/src/main/java/com/qagenic/automation/App.java
+++ demo/src/main/java/com/qagenic/automation/App.java
@@ -6,8 +6,23 @@ package com.qagenic.automation;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
+    }
+
+
+    public int getSumOfNumbers(int number1, int number2){
+        return number1 + number2;
+    }
+
+
+    public int getDivision(int numerator, int denominator){
+
+        int result;
+        if (denominator == 0){
+            return 0;
+        }        
+        result = numerator / denominator;
+        return result;
     }
 }
--- demo/src/test/java/com/qagenic/automation/AppTest.java
+++ demo/src/test/java/com/qagenic/automation/AppTest.java
@@ -1,20 +1,50 @@
 package com.qagenic.automation;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AppTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setupSeleneiumTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
+
+    @Test
+    public void testWikipedia(){
+
+        driver.get("https://www.wikipedia.com");
+
+        driver.findElement(By.name("search")).sendKeys("Selenium");
+
+        driver.findElement(By.cssSelector("button.pure-button")).click();
+
+        String headingText = driver.findElement(By.id("firstHeading")).getText();
+
+        assertEquals("Selenium", headingText);
+
+    }
+
+    @Test
+    public void testMainPageLink(){
+        
+    }
+
+    @After
+    public void closeSeleniumTest(){
+        driver.close();
+    }
+
 }
